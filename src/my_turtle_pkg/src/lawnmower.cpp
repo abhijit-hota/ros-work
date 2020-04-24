35abd9e62b65fa434df598aac226cd8f96fc1a86
abhijitXD <abhihota025@gmail.com>
2020-04-24 17:28:19 +0530

    lawnmower complete!

--- src/my_turtle_pkg/src/lawnmower.cpp
+++ src/my_turtle_pkg/src/lawnmower.cpp
@@ -2,23 +2,19 @@
 #include <geometry_msgs/Twist.h>
 #include <turtlesim/Pose.h>
 #include <iostream>
-#include <math.h>
+#define PI 3.14159265358979323846
+
+
 using namespace std;
 
 ros::Publisher velocity_publisher;
-
 ros::Subscriber pose_subscriber;
-ros::Subscriber destination_sub;
 
 turtlesim::Pose turtlesim_pose;
-
-const double PI = 3.14159265359;
-
 geometry_msgs::Twist vel_msg;
 
-double getDistance(double x1, double y1, double x2, double y2)
-{
-    return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+double radToDeg(double radian){
+    return (radian * (180 / PI));
 }
 
 void poseCallback(const turtlesim::Pose::ConstPtr &pose_message)
@@ -27,15 +23,33 @@ void poseCallback(const turtlesim::Pose::ConstPtr &pose_message)
     turtlesim_pose.y = pose_message->y;
     turtlesim_pose.theta = pose_message->theta;
 }
-void mow(){
+void mow()
+{
     ROS_INFO("%lf", 11 - turtlesim_pose.x);
-    if(11 - turtlesim_pose.x > 1 ){
-        vel_msg.linear.x = 5;
-        vel_msg.angular.z = 0;
-    } else {
-        vel_msg.linear.x = 0;
-        vel_msg.angular.z = 5;
+    bool flag = (turtlesim_pose.x == 0) ? false : true;
+
+    if (turtlesim_pose.x >= 10)
+    {
+        if (radToDeg(turtlesim_pose.theta) < 180)
+        {
+            vel_msg.linear.x = 3;
+            vel_msg.angular.z = 3;
+        }
     }
+    else if (turtlesim_pose.x < 1)
+    {
+        if (turtlesim_pose.theta > 0)
+        {
+            vel_msg.linear.x = 3;
+            vel_msg.angular.z = -3;
+        }
+    }
+    else
+    {
+        vel_msg.linear.x = 3;
+        vel_msg.angular.z = 0;
+    }
+
     velocity_publisher.publish(vel_msg);
 }
 int main(int argc, char **argv)
@@ -45,20 +59,17 @@ int main(int argc, char **argv)
     ros::NodeHandle n;
 
     ros::Rate loop_rate(30);
-    double speed, angular_speed;
-    double distance, angle;
-    bool isForward, clockwise;
 
     velocity_publisher = n.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
     pose_subscriber = n.subscribe("/turtle1/pose", 10, poseCallback);
     loop_rate.sleep();
-    
-    while(ros::ok()){
-        loop_rate.sleep();
-    	mow();
-    }
 
-    ros::spin();
+    while (ros::ok())
+    {
+        loop_rate.sleep();
+        ros::spinOnce();
+        mow();
+    }
 
     return 0;
 }
